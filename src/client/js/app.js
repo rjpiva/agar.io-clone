79725ce5989a19512fb311e7ef7e1dfa2fb46911
rjpiva <rjpiva@gmail.com>
2017-04-16 19:37:47 -0300

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -213,7 +213,7 @@ function setupSocket(socket) {
 
     socket.on('leaderboard', function (data) {
         leaderboard = data.leaderboard;
-        var status = '<span class="title">Leaderboard</span>';
+        var status = '<span class="title">Lugar dos bonzão!</span>';
         for (var i = 0; i < leaderboard.length; i++) {
             status += '<br />';
             if (leaderboard[i].id == player.id){
